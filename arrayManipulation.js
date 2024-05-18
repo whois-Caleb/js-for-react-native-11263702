1e51f9be79f700fb5485becde35efe7464c55f93
Caleb Mawusi <cemawusi@st.ug.edu.gh>
2024-05-18 15:51:37 +0000

    Added function formatArrayStrings

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,24 @@ function processArray (arr){
 
     );
 }
+
+
+function formatArrayStrings (stringArr, numArr){
+    return stringArr.map((str, index) =>{
+        processedNum = processArray(numArr);
+
+        const num = processedNum[index];
+        if (num % 2 === 0){
+            return str.toUpperCase();
+        }
+        else {
+            return str.toLowerCase();
+        }
+    }
+)
+}
+
+post = [1, 5, 6, 8, 9]
+bar = ["coOkiE", "bOok", "CRAbs", "pLayeR", "gYm"]
+
+console.log(formatArrayStrings(bar, post));
